4384aff3cd8b8606d95934265f34c0eec72ce2a9
ShyamSundarB218 <147033157+ShyamSundarB218@users.noreply.github.com>
2023-10-23 19:45:32 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oslass1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"D:\mv\osl1.txt";
+                string[] lines2 = File.ReadAllLines(path);
+                Console.WriteLine("The Students Data are \n");
+                int index = 1;
+                foreach (string line in lines2)
+                    Console.WriteLine($"  {index++} : " + line);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("All the Students data in the files are Displayed.");
+
+            }
+        
+        Console.ReadLine();
+        }
+
+    }
+}
